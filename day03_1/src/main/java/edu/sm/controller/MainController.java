d2da2c0798b9f66bcb60c3edd2a39c8fd199e475
Hyoseok Seo <seohyoseok0408@gmail.com>
2024-10-09 12:25:25 +0900

    1008: bootstrap

---
+++ day03_1/src/main/java/edu/sm/controller/MainController.java
@@ -0,0 +1,17 @@
+package edu.sm.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class MainController {
+
+    @RequestMapping("/")
+    public String main(Model model) {
+        return "index";
+    }
+
+}
